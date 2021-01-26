4a17c03d2957b232d980afa0143738ea6f99bb65
andrijunaedi <andrijunaedi126@gmail.com>
2021-01-26 21:32:17 +0700

    deploy

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app
-  .get('/', (req, res) => res.send('Hello World..'))
+  .get('/', (req, res) => res.send('Belajar CI/CD..'))
   .listen(5000, () => console.log('server live...'));
