20b1d3109639d16a283d77f044c980c6720a89cc
andrijunaedi <andrijunaedi126@gmail.com>
2021-01-26 20:20:52 +0700

    Bismillah

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app
-  .get('/', (req, res) => res.send('Hello Word'))
+  .get('/', (req, res) => res.send('Bissmillah..'))
   .listen(5000, () => console.log('server live...'));
