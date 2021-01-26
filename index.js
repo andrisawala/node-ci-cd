44d6011250876ade00ff4dcc6671dbd6f56f0d00
andrijunaedi <andrijunaedi126@gmail.com>
2021-01-26 22:00:09 +0700

    finally

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app
-  .get('/', (req, res) => res.send('Lieur CI/CD..'))
+  .get('/', (req, res) => res.send('Akhirnya bisa guys.. CI/CD..'))
   .listen(3000, () => console.log('server live...'));
