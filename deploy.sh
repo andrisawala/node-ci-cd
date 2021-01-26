fdcd75ec73fd2e3474d7db6531ee76a1d20dc04b
andrijunaedi <andrijunaedi126@gmail.com>
2021-01-26 21:35:07 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 echo "Kill all the running PM2 actions"
-pm2 kill
+sudo pm2 kill
 
 echo "Update app from Git"
 git pull
 
 echo "Install app dependencies"
 sudo rm -rf node_modules yarn-lock.json
-yarn install
+sudo yarn install
 
 echo "Run PM2 Server"
-pm2 start index.js --name test
+sudo pm2 start index.js --name test
