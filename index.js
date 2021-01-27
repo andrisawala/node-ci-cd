4e210a87923d17807d8bd9b61068a3a27fdd748f
andrijunaedi <andrijunaedi126@gmail.com>
2021-01-27 10:14:53 +0700

    deploy with SSH

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app
-  .get('/', (req, res) => res.send('Akhirnya bisa guys.. CI/CD..'))
+  .get('/', (req, res) => res.send('Coba pake SSH.. CI/CD..'))
   .listen(3000, () => console.log('server live...'));
