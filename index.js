196fd647ffd0de33fe0e08750b2cbce172d833cd
andrijunaedi <andrijunaedi126@gmail.com>
2021-01-27 10:23:25 +0700

    deploy

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app
-  .get('/', (req, res) => res.send('Coba pake SSH.. CI/CD..'))
+  .get('/', (req, res) => res.send('Test SSH.. CI/CD..'))
   .listen(3000, () => console.log('server live...'));
