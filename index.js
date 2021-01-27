d4d5f507865d12708363d9aa78cfdf457c30ef9c
andrijunaedi <andrijunaedi126@gmail.com>
2021-01-27 10:37:37 +0700

    deploy

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app
-  .get('/', (req, res) => res.send('Test SSH.. CI/CD..'))
+  .get('/', (req, res) => res.send('Bissmillah bisa SSH.. CI/CD..'))
   .listen(3000, () => console.log('server live...'));
